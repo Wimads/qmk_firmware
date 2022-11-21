d48f2e9e9fc9a74df3f71d1167fd180c82956851
Wimads <wimadstegge@gmail.com>
2022-11-21 01:49:34 +0100

    revised keymap

--- keyboards/bastardkb/charybdis/3x5/keymaps/Wimads/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/Wimads/keymap.c
@@ -41,41 +41,32 @@
 enum charybdis_keymap_layers {
     _DEF = 0, //default
     _NUM = 1, //numbers and symbols
-    _NAV = 2, //navigation and mouse keys
-    _FUN = 3,  //function keys
-    _SET = 4 //settings
+    _NAV = 2, //navigation and F-keys
+    _SET = 3  //settings and misc
 };
 
 //Custom Keycodes
 enum custom_keycodes {
     //Switch windows keyboard language
     LANGC = CHARYBDIS_SAFE_RANGE, //correction for language tracker
-    CLEARKB, //clears all registered keycodes, to solve stuck keycodes/layers/modifiers
-    //modified mouse buttons
-    M_A1, M_A2,
-    M_AS1, M_AS2,
-    M_CS1, M_CS2,
-    M_CA1, M_CA2,
-    M_MEH1, M_MEH2
+    CLEARKB //clears all registered keycodes, to solve stuck keycodes/layers/modifiers
 };
 //language key
 #define LANG LGUI(KC_SPC)
 //tap-hold keys
 #define TH_A LT(10, KC_A)
+#define TH_S LT(10, KC_S)
+#define TH_D LT(10, KC_D)
+#define TH_E LT(10, KC_E)
 #define TH_QUOT LT(10, KC_QUOT)
 #define TH_5 LT(10, KC_5)
 #define TH_8 LT(10, KC_8)
-#define TH_PDOT LT(10, KC_DOT)
+#define TH_PDOT LT(10, KC_PDOT)
 #define TH_EQL LT(10, KC_EQL)
+
 #define DEL_NAV LT(_NAV, KC_DEL)
 #define BSP_NUM LT(_NUM, KC_BSPC)
-#define SPC_SET LT(_SET, KC_SPC)
-//mouse buttons
-#define M_1 KC_BTN1
-#define M_2 KC_BTN2
-#define M_3 KC_BTN3
-#define M_4 KC_BTN4
-#define M_5 KC_BTN5
+#define CAP_SFT LSFT_T(KC_CAPS)
 
 #include "g/keymap_combo.h"
 
@@ -83,61 +74,49 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DEF] = LAYOUT(
   // ╭────────────────────────────────────────────╮                   ╭─────────────────────────────────────────────╮
-      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+      KC_Q,    KC_W,    TH_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
   // ├────────────────────────────────────────────┤                   ├─────────────────────────────────────────────┤
-      TH_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    TH_QUOT,
+      TH_A,    TH_S,    TH_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    TH_QUOT,
   // ├────────────────────────────────────────────┤                   ├─────────────────────────────────────────────┤
       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_EXLM,
   // ╰─────────────────────────────────────────────────────╮  ╭─────────────────────────────────────────────────────╯
-               M_1,     M_2,     DEL_NAV, KC_LSFT, KC_LCTL,    KC_LALT, KC_SPC,  BSP_NUM   //trackball = mouse
+             KC_BTN1, KC_BTN2,   KC_LALT, KC_LSFT, KC_LCTL,    DEL_NAV, KC_SPC,  BSP_NUM   //trackball = mouse
   //        ╰─────────────────╯ ╰──────────────────────────╯  ╰──────────────────────────╯
   ),
 
   [_NUM] = LAYOUT(
   // ╭────────────────────────────────────────────╮                   ╭─────────────────────────────────────────────╮
-      KC_ASTR, KC_7,    TH_8,    KC_9,    KC_PPLS,                      KC_DLR,  KC_UNDS, KC_LPRN, KC_RPRN, KC_PERC,
+      KC_ASTR, KC_7,    TH_8,    KC_9,    KC_PLUS,                      KC_DLR,  KC_UNDS, KC_LPRN, KC_RPRN, KC_PERC,
   // ├────────────────────────────────────────────┤                   ├─────────────────────────────────────────────┤
-      KC_PSLS, KC_4,    TH_5,    KC_6,    KC_PMNS,                      KC_HASH, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR,
+      KC_SLSH, KC_4,    TH_5,    KC_6,    KC_MINS,                      KC_HASH, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR,
   // ├────────────────────────────────────────────┤                   ├─────────────────────────────────────────────┤
       KC_CIRC, KC_1,    KC_2,    KC_3,    TH_EQL,                       KC_PIPE, KC_AMPR, KC_LT,   KC_GT,   KC_BSLS,
   // ╰─────────────────────────────────────────────────────╮  ╭─────────────────────────────────────────────────────╯
-             KC_0,    TH_PDOT,   KC_RALT, KC_RSFT, KC_RCTL,    KC_LALT, MO(_FUN), _______   //trackball = scroll
+             KC_0,    TH_PDOT,   _______, CAP_SFT, _______,    KC_RCTL, KC_RSFT, _______   //trackball = scroll
   //        ╰─────────────────╯ ╰──────────────────────────╯  ╰──────────────────────────╯
   ),
 
   [_NAV] = LAYOUT(
   // ╭────────────────────────────────────────────╮                   ╭─────────────────────────────────────────────╮
-      M_MEH1,  M_CA1,   M_CA2,   M_MEH2,  XXXXXXX,                      KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PSCR,
+      KC_BRIU, KC_F7,   KC_F8,   KC_F9,   XXXXXXX,                      KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU,
   // ├────────────────────────────────────────────┤                   ├─────────────────────────────────────────────┤
-      XXXXXXX, M_CS1,   M_CS2,   XXXXXXX, XXXXXXX,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,
+      KC_BRID, KC_F4,   KC_F5,   KC_F6,   XXXXXXX,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD,
   // ├────────────────────────────────────────────┤                   ├─────────────────────────────────────────────┤
-      M_4,     M_AS1,   M_AS2,   M_5,     SNIPING,                      KC_NUM,  KC_MENU, KC_CAPS, KC_BRK,  KC_SCRL,
+      KC_F10,  KC_F1,   KC_F2,   KC_F3,   XXXXXXX,                      XXXXXXX, KC_BTN4, KC_F5,   KC_BTN5, KC_MUTE,
   // ╰─────────────────────────────────────────────────────╮  ╭─────────────────────────────────────────────────────╯
-               M_A1,    M_A2,    _______, SPC_SET, CLEARKB,    KC_LCTL, KC_LSFT, DRG_TOG   //trackball = mouse
-  //        ╰─────────────────╯ ╰──────────────────────────╯  ╰──────────────────────────╯
-  ),
-
-  [_FUN] = LAYOUT(
-  // ╭────────────────────────────────────────────╮                   ╭─────────────────────────────────────────────╮
-      XXXXXXX, KC_F7,   KC_F8,   KC_F9,   XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  // ├────────────────────────────────────────────┤                   ├─────────────────────────────────────────────┤
-      XXXXXXX, KC_F4,   KC_F5,   KC_F6,   XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  // ├────────────────────────────────────────────┤                   ├─────────────────────────────────────────────┤
-      XXXXXXX, KC_F1,   KC_F2,   KC_F3,   XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  // ╰─────────────────────────────────────────────────────╮  ╭─────────────────────────────────────────────────────╯
-             KC_F10,   KC_F11,   KC_F12,  XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX   //trackball = scroll
+              KC_F11,  KC_F12,   _______, KC_LSFT, _______,    _______, XXXXXXX, DRG_TOG   //trackball = sniping mouse
   //        ╰─────────────────╯ ╰──────────────────────────╯  ╰──────────────────────────╯
   ),
 
   [_SET] = LAYOUT(
   // ╭────────────────────────────────────────────╮                   ╭─────────────────────────────────────────────╮
-      XXXXXXX, KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
+      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_INS,  XXXXXXX, KC_PSCR,
   // ├────────────────────────────────────────────┤                   ├─────────────────────────────────────────────┤
-      XXXXXXX, KC_VOLU, KC_BRIU, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      CLEARKB, KC_SCRL, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├────────────────────────────────────────────┤                   ├─────────────────────────────────────────────┤
-      XXXXXXX, KC_VOLD, KC_BRID, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, KC_CAPS, XXXXXXX, KC_BRK,                       KC_NUM,  KC_MENU, XXXXXXX, XXXXXXX, XXXXXXX,
   // ╰─────────────────────────────────────────────────────╮  ╭─────────────────────────────────────────────────────╯
-             XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX   //trackball = scroll
+             XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX   //trackball = mouse
   //        ╰─────────────────╯ ╰──────────────────────────╯  ╰──────────────────────────╯
   ),
 
@@ -149,8 +128,8 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case DEL_NAV:   return true;
         case BSP_NUM:   return true;
-        case MO(_FUN):  return true;
-        case SPC_SET:   return true;
+        case MO(_SET):  return true;
+        case CAP_SFT:   return true;
         default:        return false; // disable permissive hold for TH keys
     }
 };
@@ -158,8 +137,8 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case DEL_NAV:   return TAPPING_TERM;
         case BSP_NUM:   return TAPPING_TERM;
-        case MO(_FUN):  return TAPPING_TERM;
-        case SPC_SET:   return TAPPING_TERM;
+        case MO(_SET):  return TAPPING_TERM;
+        case CAP_SFT:   return TAPPING_TERM;
         default:        return 225; // set tapping term for TH keys
     }
 };
@@ -167,8 +146,8 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case DEL_NAV:   return false;
         case BSP_NUM:   return false;
-        case MO(_FUN):  return false;
-        case SPC_SET:   return false;
+        case MO(_SET):  return false;
+        case CAP_SFT:   return false;
         default:        return true; // enable tapping force hold for TH keys
     }
 };
@@ -179,12 +158,17 @@ typedef struct _taphold_keycode_t { //define taphold functions
     uint16_t keycode_hold; //key output on hold
 } taphold_keycode_t;
 
-#define KEY_MAP_SIZE 6 //amount of keys in taphold map (30 on _DEF, 30 on _NUM)
+#define KEY_MAP_SIZE 9 //amount of keys in taphold map (30 on _DEF, 30 on _NUM)
 
 taphold_keycode_t keymap[KEY_MAP_SIZE] = { // taphold mapping, format: {keycode_record, keycode_tap, keycode_hold}
     {TH_A,    KC_AT},
+    {TH_S,    SNIPING},
+    {TH_D,    KC_DLR},
+
+    {TH_E,    KC_NO},
     {TH_QUOT, KC_GRV},
     {TH_EQL,  KC_TILD},
+
     {TH_PDOT, KC_COMM},
     {TH_5,    KC_DLR},
     {TH_8,    KC_NO},
@@ -231,20 +215,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) { //execute auts
                 register_mods(mod_shift);
                 return false;
             } return true;
+
+        //trackball sniping scrolling:
         case DEL_NAV:
-            if (record->event.pressed && record->tap.count && mod_shift){ //override when tapped and shifted
-                unregister_mods(mod_shift);
-                tap_code16(KC_BSPC);
-                register_mods(mod_shift);
-                return false;
-            } return true; //else don't override
+            if (record->event.pressed) {
+                charybdis_set_pointer_sniping_enabled(true);
+            } else {
+                charybdis_set_pointer_sniping_enabled(false);
+            } return true;
         case BSP_NUM:
-            if (record->event.pressed && record->tap.count && mod_shift) {
-                unregister_mods(mod_shift);
-                tap_code16(KC_BSPC);
-                register_mods(mod_shift);
-                return false;
-            } else if (record->event.pressed) {
+            if (record->event.pressed) {
                 charybdis_set_pointer_dragscroll_enabled(true);
             } else {
                 charybdis_set_pointer_dragscroll_enabled(false);
@@ -260,120 +240,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) { //execute auts
                 language_EN = !language_EN;
             } return false;
 
-        //Mouse buttons
-        case M_A1:
-            if (record->event.pressed) {
-                register_code16(KC_RALT);
-                wait_ms(10);
-                register_code16(KC_BTN1);
-            } else {
-                unregister_code16(KC_BTN1);
-                wait_ms(10);
-                unregister_code16(KC_RALT);
-            } return false;
-        case M_A2:
-            if (record->event.pressed) {
-                register_code16(KC_RALT);
-                wait_ms(10);
-                register_code16(KC_BTN2);
-            } else {
-                unregister_code16(KC_BTN2);
-                wait_ms(10);
-                unregister_code16(KC_RALT);
-            } return false;
-        case M_AS1:
-            if (record->event.pressed) {
-                register_code16(KC_RALT);
-                register_code16(KC_LSFT);
-                register_code16(KC_BTN1);
-            } else {
-                unregister_code16(KC_BTN1);
-                wait_ms(10);
-                unregister_code16(KC_RALT);
-                unregister_code16(KC_LSFT);
-            } return false;
-        case M_AS2:
-            if (record->event.pressed) {
-                register_code16(KC_RALT);
-                register_code16(KC_LSFT);
-                register_code16(KC_BTN2);
-            } else {
-                unregister_code16(KC_BTN2);
-                wait_ms(10);
-                unregister_code16(KC_RALT);
-                unregister_code16(KC_LSFT);
-            } return false;
-        case M_CS1:
-            if (record->event.pressed) {
-                register_code16(KC_LCTL);
-                register_code16(KC_LSFT);
-                register_code16(KC_BTN1);
-            } else {
-                unregister_code16(KC_BTN1);
-                wait_ms(10);
-                unregister_code16(KC_LCTL);
-                unregister_code16(KC_LSFT);
-            } return false;
-        case M_CS2:
-            if (record->event.pressed) {
-                register_code16(KC_LCTL);
-                register_code16(KC_LSFT);
-                register_code16(KC_BTN2);
-            } else {
-                unregister_code16(KC_BTN2);
-                wait_ms(10);
-                unregister_code16(KC_LCTL);
-                unregister_code16(KC_LSFT);
-            } return false;
-        case M_CA1:
-            if (record->event.pressed) {
-                register_code16(KC_LCTL);
-                register_code16(KC_RALT);
-                register_code16(KC_BTN1);
-            } else {
-                unregister_code16(KC_BTN1);
-                wait_ms(10);
-                unregister_code16(KC_LCTL);
-                unregister_code16(KC_RALT);
-            } return false;
-        case M_CA2:
-            if (record->event.pressed) {
-                register_code16(KC_LCTL);
-                register_code16(KC_RALT);
-                register_code16(KC_BTN2);
-            } else {
-                unregister_code16(KC_BTN2);
-                wait_ms(10);
-                unregister_code16(KC_LCTL);
-                unregister_code16(KC_RALT);
-            } return false;
-        case M_MEH1:
-            if (record->event.pressed) {
-                register_code16(KC_LCTL);
-                register_code16(KC_RALT);
-                register_code16(KC_LSFT);
-                register_code16(KC_BTN1);
-            } else {
-                unregister_code16(KC_BTN1);
-                wait_ms(10);
-               unregister_code16(KC_LCTL);
-               unregister_code16(KC_RALT);
-               unregister_code16(KC_LSFT);
-            } return false;
-        case M_MEH2:
-            if (record->event.pressed) {
-                register_code16(KC_LCTL);
-                register_code16(KC_RALT);
-                register_code16(KC_LSFT);
-                register_code16(KC_BTN2);
-            } else {
-                unregister_code16(KC_BTN2);
-                wait_ms(10);
-                unregister_code16(KC_LCTL);
-                unregister_code16(KC_RALT);
-                unregister_code16(KC_LSFT);;
-            } return false;
-
         //taphold keys:
         default:
             if (index != -1) { //if key is in taphold map, then custom tap/hold behaviour below:
@@ -384,11 +250,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) { //execute auts
                         } else {
                             tap_code16(C(A(KC_E))); //€ sign on english(us) layout
                         }
+                    } else if (keymap[index].keycode_hold == SNIPING) {
+                        charybdis_set_pointer_sniping_enabled(true);
                     } else {
-                    tap_code16(keymap[index].keycode_hold);
+                        tap_code16(keymap[index].keycode_hold);
                     }
                     return false;
-                } return true; //if tap, return true
+                } else if (record->event.pressed) { //tap action
+                    return true;
+                } else { //key release
+                    charybdis_set_pointer_sniping_enabled(false);
+                    return true;
+                }
 
                 return false; //if key is in taphold map, ignore original key behaviour
             }//end of if(index != -1)
